0efeb5fee06c6006409920a3f0889cb2edaba44b
FLtower <francois.louis@live.fr>
2025-05-27 18:06:37 +0200

    Check if stun works and check on target change

--- Main.lua
+++ Main.lua
@@ -10,6 +10,7 @@
 
 -- A is addon name string and N is namespace table
 local A, N = ...
+local Presets = N.Presets
 local Utils = N.Utils
 
 -- Main frame for this addon
@@ -24,6 +25,28 @@ N.DEBUG = true
 -- Functions
 -----------------------------
 
+-- Function used to check if target is stunnable
+--- @param guid? string GUID of unit to test
+local function IsStunnable(guid)
+    if not guid then return end
+
+    if not StunnableDB or not StunnableDB.Mobs then return end
+    local value = StunnableDB.Mobs[guid]
+    Utils.PrintMsgDebug("--> IsStunnable " .. guid .. ": " .. (value and "true" or "false"))
+end
+
+-- Function used to save mob stunnable status
+--- @param guid? string GUID of unit to test
+local function SaveMob(guid, value)
+    if not guid then return end
+
+    Utils.PrintMsgDebug("--> SaveMob " .. guid .. ": " .. (value and "true" or "false"))
+
+    if not StunnableDB then StunnableDB = {} end
+    if not StunnableDB.Mobs then StunnableDB.Mobs = {} end
+    StunnableDB.Mobs[guid] = value
+end
+
 -- Callback function after PLAYER_LOGIN event
 local function OnPlayerLogin()
     Utils.PrintMsgDebug("--> OnPlayerLogin")
@@ -32,11 +55,31 @@ end
 -- Callback function after PLAYER_TARGET_CHANGED event
 local function OnPlayerTargetChanged()
     Utils.PrintMsgDebug("--> OnPlayerTargetChanged")
+    if UnitExists("target") and UnitCanAttack("player", "target") then
+        local targetGUID = UnitGUID("target")
+        IsStunnable(targetGUID)
+    end
 end
 
 -- Callback function after COMBAT_LOG_EVENT_UNFILTERED event
-local function OnCombatLogEventUnfiltered()
-    Utils.PrintMsgDebug("--> OnCombatLogEventUnfiltered")
+--- @param subEvent string subEvent
+--- @param destGUID string GUID of unit to gets the aura
+--- @param spellID? string ID of the spell
+local function OnCombatLogEventUnfiltered(subEvent, destGUID, spellID)
+    -- Utils.PrintMsgDebug("--> OnCombatLogEventUnfiltered")
+
+    if not subEvent or not destGUID or not spellID then return end
+    if subEvent ~= "SPELL_AURA_APPLIED" and subEvent ~= "SPELL_MISSED" then return end
+    if not Utils.TableHasValue(Presets.Spells, tonumber(spellID)) then return end
+
+    if subEvent == "SPELL_AURA_APPLIED" then
+        Utils.PrintMsgDebug("Spell aura applied " .. spellID .. " to " .. destGUID)
+        SaveMob(destGUID, true)
+    end
+    if subEvent == "SPELL_MISSED" then
+        Utils.PrintMsgDebug("Spell missed " .. spellID .. " to " .. destGUID)
+        SaveMob(destGUID, false)
+    end
 end
 
 -- Called by slash command
@@ -63,7 +106,7 @@ frame = CreateFrame("FRAME", "Stunnable", UIParent)
 frame:RegisterEvent("PLAYER_LOGIN") -- Fired on connection/reload
 frame:RegisterEvent("PLAYER_TARGET_CHANGED") -- Fired whenever the player's target is changed
 frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED") -- Used to parse all events the moment they happen
-frame:SetScript("OnEvent", function(self, event, ...)
+frame:SetScript("OnEvent", function(self, event)
 	if event == "PLAYER_LOGIN" then
         Utils.PrintMsgDebug("--> Event PLAYER_LOGIN")
 		OnPlayerLogin()
@@ -73,7 +116,8 @@ frame:SetScript("OnEvent", function(self, event, ...)
 		OnPlayerTargetChanged()
 	end
     if event == "COMBAT_LOG_EVENT_UNFILTERED" then
-        Utils.PrintMsgDebug("--> Event COMBAT_LOG_EVENT_UNFILTERED")
-		OnCombatLogEventUnfiltered()
+        -- Utils.PrintMsgDebug("--> Event COMBAT_LOG_EVENT_UNFILTERED")
+        local _, subEvent, _, _, _, _, _, destGUID, _, _, _, spellID = CombatLogGetCurrentEventInfo()
+		OnCombatLogEventUnfiltered(subEvent, destGUID, spellID)
 	end
 end)
---
+++ Utils.lua
@@ -0,0 +1,49 @@
+-----------------------------
+-- Variables
+-----------------------------
+
+local A, N = ...
+local Assets = N.Assets
+N.Utils = {}
+
+-----------------------------
+-- Functions
+-----------------------------
+
+-- Write message on default chat with prefix
+--- @param str string string to write
+--- @param isDebug boolean is debug enabled
+local function PrintMsg(str, isDebug)
+	print("[|cff91ffdc" .. A .. (isDebug and " (Debug)" or "") .. "|r] : " .. str)
+end
+N.Utils.PrintMsg = PrintMsg
+
+-- Write message on default chat with prefix if debug is enabled
+--- @param str string string to write
+local function PrintMsgDebug(str)
+	if N.DEBUG then PrintMsg(str, true) end
+end
+N.Utils.PrintMsgDebug = PrintMsgDebug
+
+-- Add color, blizzard way, to string
+--- @param str string string to write
+--- @param color string color in hexa foramt argb
+--- @return string
+local function StrInColor(str, color)
+	return "|c" .. color .. str .. "|r"
+end
+N.Utils.StrInColor = StrInColor
+
+-- Check if a value is in a table
+--- @param tab table containing table
+--- @param val any the value to check
+--- @return boolean
+local function TableHasValue (tab, val)
+    for _, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
+end
+N.Utils.TableHasValue = TableHasValue
