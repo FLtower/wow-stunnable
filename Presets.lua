f50d8247996b82943c6c775f8967476a7a107347
FLtower <francois.louis@live.fr>
2025-05-28 23:17:41 +0200

    Add spells

--- Presets.lua
+++ Presets.lua
@@ -10,6 +10,12 @@ N.Presets = {}
 -----------------------------
 
 N.Presets.Spells = {
+    { spellId = 853, auraId = 853 }, -- Paladin - Hammer of Justice
     { spellId = 107570, auraId = 132168 }, -- Warrior - Shockwave
-    { spellId = 46968, auraId = 132169 } -- Warrior - Storm Bolt
+    { spellId = 46968, auraId = 132169 }, -- Warrior - Storm Bolt
+    { spellId = 408, auraId = 408 }, -- Rogue - Kidney Shot
+    { spellId = 474421, auraId = 24394 }, -- Hunter - Intimidation
+    { spellId = 119381, auraId = 119381 }, -- Monk - Leg Sweep
+    { spellId = 192058, auraId = 118905 }, -- Shaman - Capacitor Totem
+    { spellId = 221562, auraId = 221562 } -- DK - Asphyxiate
 }
